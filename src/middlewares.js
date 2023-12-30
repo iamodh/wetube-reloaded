0ba1e9a4658844ba5f4d02c3684aca12233fdff9
iamodh <68431235+iamodh@users.noreply.github.com>
2023-12-31 00:02:01 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "we-tubee",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -15,7 +31,7 @@ export const protectorMiddleware = (req, res, next) => {
     return res.redirect("/login");
   }
 };
-  
+
 export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     next();
@@ -28,8 +44,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 10000000 },
+  storage: multerUploader,
 });
